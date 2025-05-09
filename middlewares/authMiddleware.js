e3a20ab34f228c569d8e48a57d9ca22e2bd603c4
Yoga Fikri <95378690+Ago-oyu@users.noreply.github.com>
2025-05-09 21:44:37 +0700

    Improve authorization error handling and update user assignment in authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -8,10 +8,24 @@ const authorize = async (req, res, next) => {
         if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
             token = req.headers.authorization.split(' ')[1]
         } else {
-            res.status(401).json({ message: 'unauthorized' })
+            res.status(401).json({ message: 'unauthorized, access token is missing' })
         }
 
-        const payload = jwt.decode(token, { json: true })
+        // const payload = jwt.decode(token, { json: true })
+
+        const decoded = jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+            if (err) {
+                if (err.name === 'TokenExpiredError') {
+                    res.status(401).json({
+                        message: 'token has expired'
+                    })
+                } else {
+                    res.status(401).json({
+                        message: 'invalid token'
+                    })
+                }
+            }
+        })
 
         const user = (await db.query('SELECT * FROM users WHERE id = $1', [payload.userId])).rows[0]
 
@@ -19,7 +33,7 @@ const authorize = async (req, res, next) => {
             res.status(401).json({ message: 'unauthorized' })
         }
         // console.log(user);
-        req.user = user
+        req.user = decoded;
         next()
     } catch (error) {
         console.log(error)
